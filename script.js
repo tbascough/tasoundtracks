669be60892e9c37cdabac2c1ea62f1aa05690b34
Thomas Ascough <tascough@Missy.local>
2022-03-31 15:47:15 -0700

    hope this works

--- script.js
+++ script.js
@@ -9,39 +9,12 @@ function displayHeader(pagename){
 		</div>
 
 		<div class="socialbox">
-			<a class="aimg" href="https://www.youtube.com/channel/UCwp5daz8CNNN0pIeCs3Q0JQ" target="_blank" rel="noopener noreferrer"><img src="imgs/icon_yt.png" class="icon"><a href="https://www.youtube.com/channel/UCwp5daz8CNNN0pIeCs3Q0JQ" target="_blank" rel="noopener noreferrer"></a>
 			<a class="aimg" href="https://www.facebook.com/tommy.ascough" target="_blank" rel="noopener noreferrer"><img src="imgs/icon_fb.webp" class="icon"></a>
+			<a class="aimg" href="https://www.youtube.com/channel/UCwp5daz8CNNN0pIeCs3Q0JQ" target="_blank" rel="noopener noreferrer"><img src="imgs/icon_yt.png" class="icon"><a href="https://www.youtube.com/channel/UCwp5daz8CNNN0pIeCs3Q0JQ" target="_blank" rel="noopener noreferrer"></a>
 			<a class="aimg" href="https://open.spotify.com/artist/5f2waN8ZNrblaZEvfzHPeg?si=vp0mYb5lQfuaERpP5kegFQ" target=_blank" rel="noopener noreferrer"><img src="imgs/icon_spotify.png" class="icon"></a>
 		</div>
 	`
 }
-
-
-
-// function displayNav(){
-// 	var navBar = document.getElementById('navBar')
-// 	navBar.innerHTML =`
-// 			<nav>
-// 				<a class="navbutton" href="./index.html">News</a>
-// 				<a class="navbutton" href="./mus.html">Music</a>
-// 				<a class="navbutton" href="./sd.html">Sound Design</a>
-// 				<a class="navbutton" href="./about.html">About</a>
-// 			</nav>
-// 	`
-// }
-
-// function displayNav2(){
-// 	var navBar = document.getElementById('navBar')
-// 	var cbfilms = document.getElementById('cbfilms')
-// 	navBar.innerHTML =`
-// 			<nav>
-// 				<a class="navbutton" href="./index.html">Games</a>
-// 				<a class="navbutton" href="./mus.html">Music</a>
-// 				<a class="navbutton" href="${cbfilms}">Film</a>
-// 				<a class="navbutton" href="./sd.html">About</a>
-// 			</nav>
-// 	`
-// }
 
 
 
